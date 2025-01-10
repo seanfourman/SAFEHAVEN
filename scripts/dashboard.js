da251fad5ee3fcb8ecda6b1d75fff79c1bab8d4e
seanfourman <Sean.Fourman@Ruppin365.net>
2025-01-10 20:50:26 +0200

    added email and logout button

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,6 +1,20 @@
+const emailAddress = localStorage.getItem("loggedUser");
+const username = emailAddress.split("@")[0];
+
 document.addEventListener("DOMContentLoaded", function () {
   if (!localStorage.getItem("loggedUser")) {
     alert("You are not logged in. Redirecting to the home page.");
     window.location.href = "./Home.html";
   }
+  changeConnectedEmail();
 });
+
+function changeConnectedEmail() {
+  document.getElementById("emailAddress").innerHTML = emailAddress;
+  document.getElementById("logout").addEventListener("click", logout);
+}
+
+function logout() {
+  localStorage.removeItem("loggedUser");
+  window.location.href = "./Home.html";
+}
