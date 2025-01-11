22c2e70d50a2a49a762ff7d1c5b8b55d560c7fff
avivmoshe11 <avivmoshe123@gmail.com>
2025-01-11 03:14:28 +0200

    my changes

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,153 +1,174 @@
-// Constants
-const MINIMUM_AGE = 16;
-const USERS_LIST_KEY = "listOfUsers";
+class CustomForm {
+  constructor(formElement) {
+    this.$form = formElement;
+    this._init();
 
-/* FORM VALIDATION */
-// Input specific validation functions
-function validateEmail(input) {
-  const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
-  return regex.test(input);
-}
-function validatePassword(input) {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{8}$/;
-  return regex.test(input);
-}
-function validateBirthdate(input) {
-  const today = new Date();
-  const age = today.getFullYear() - new Date(input).getFullYear(); // check if value empty
-  return age >= MINIMUM_AGE; // maybe add after
-}
-function validateCardNumber(input) {
-  input = input.replaceAll(" ", ""); // check empty
-  const regex = /^\d{16}$/;
-  return regex.test(input);
-}
-function validateExpirationDate(input) {
-  if (!input) return false;
-  const expirationDate = new Date(input + "-01");
-  const thisMonth = new Date();
-  thisMonth.setDate(1);
-  thisMonth.setHours(0, 0, 0, 0);
-  return expirationDate >= thisMonth;
-}
-function validateCVV(input) {
-  const regex = /^\d{3}$/;
-  return regex.test(input);
-}
-
-// Form validation dictionary and errors
-const validationDictionary = {
-  email: { method: validateEmail, error: "Please enter a valid email address." },
-  password: { method: validatePassword, error: "Password must be 8 characters long, must include a-z, A-Z, 0-9, special character" },
-  birthdate: { method: validateBirthdate, error: `Must be at least ${MINIMUM_AGE} years old` },
-  cardNumber: { method: validateCardNumber, error: "Card number must be 16 digits" },
-  expirationDate: { method: validateExpirationDate, error: "Expiration date must be valid" },
-  cvv: { method: validateCVV, error: "CVV must be exactly 3 digits" }
-};
-
-// Set error
-function setErrorMessage(inputElement, message) {
-  let errorSpan = inputElement.parentElement.querySelector(".error-message");
-  if (!errorSpan) {
-    errorSpan = document.createElement("span");
-    errorSpan.classList.add("error-message");
-    inputElement.parentElement.appendChild(errorSpan);
+    this._errorHandler = new CustomFormErrorHandler(this);
+    this._validator = new CustomFormValidator(this, this._errorHandler);
+    this._actionHandler = new CustomFormActionHandler(this, this._errorHandler);
+    this._bindEventListeners();
   }
-  errorSpan.textContent = message;
-  inputElement.classList.add("error");
-}
 
-function clearErrorMessage(inputElement) {
-  let errorSpan = inputElement.parentElement.querySelector(".error-message");
-  if (errorSpan) errorSpan.remove();
-  inputElement.classList.remove("error");
-}
-
-// Form validation handlers
-function validateFormInput(inputElement) {
-  const name = inputElement.name;
-  const validation = validationDictionary[name];
-  if (!validation) return false;
-
-  const valid = validation.method(inputElement.value);
-  valid ? clearErrorMessage(inputElement) : setErrorMessage(inputElement, validation.error);
-  return valid;
-}
-
-function validateForm(form) {
-  let isValid = true;
-  const inputs = form.querySelectorAll("input");
-
-  inputs.forEach((input) => {
-    if (!validateFormInput(input)) {
-      isValid = false;
+  _init() {
+    this.$inputs = Array.from(this.$form.querySelectorAll("input"));
+    this.$submitBtn = this.$form.querySelector('button[type="submit"]');
+  }
+  _bindEventListeners() {
+    this.$form.addEventListener("submit", (event) => this._handleSubmit(event));
+    this.$form.addEventListener("change", (event) => this._validator.validateIfInput(event));
+    this.$form.addEventListener("focusout", (event) => this._validator.validateIfInput(event));
+  }
+  _handleSubmit(event) {
+    event.preventDefault();
+    if (this._validator.validateForm()) {
+      this._actionHandler.handleFormAction();
     }
-  });
-  return isValid;
-}
-
-function validateIfInput(event) {
-  if (event.target.tagName === "INPUT") {
-    validateFormInput(event.target);
   }
 }
 
-function attachFormsFunctionality(form) {
-  const submitButton = form.querySelector('button[type="submit"]');
+class CustomFormValidator {
+  static _minimumAge = 16;
+  static _dictionary = {
+    email: { method: CustomFormValidator._validateEmail, error: "Please enter a valid email address." },
+    password: { method: CustomFormValidator._validatePassword, error: "Password must be 8 characters and include a-z, A-Z, 0-9, special character" },
+    birthdate: { method: CustomFormValidator._validateBirthdate, error: `Must be at least ${CustomFormValidator._minimumAge} years old` },
+    cardNumber: { method: CustomFormValidator._validateCardNumber, error: "Card number must be 16 digits" },
+    expirationDate: { method: CustomFormValidator._validateExpirationDate, error: "Expiration date must be valid" },
+    cvv: { method: CustomFormValidator._validateCVV, error: "CVV must be exactly 3 digits" }
+  };
 
-  if (submitButton) {
-    submitButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      if (validateForm(form)) {
-        handleFormAction(form);
+  constructor(customForm, errorHandler) {
+    this._customForm = customForm;
+    this._errorHandler = errorHandler;
+  }
+
+  validateForm() {
+    return this._customForm.$inputs.every((inputElement) => this.validateInput(inputElement));
+  }
+  validateInput(inputElement) {
+    const name = inputElement.name;
+    const validation = CustomFormValidator._dictionary[name];
+    if (!validation) return false;
+
+    const valid = validation.method(inputElement.value);
+    valid ? this._errorHandler.clearInputErrorMessage(inputElement) : this._errorHandler.setInputErrorMessage(inputElement, validation.error);
+    return valid;
+  }
+  validateIfInput(event) {
+    if (event.target.localName === "input") {
+      this.validateInput(event.target);
+    }
+  }
+  static _validateEmail(value) {
+    const regex = /^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+    return regex.test(value);
+  }
+  static _validatePassword(value) {
+    const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&#]{8}$/;
+    const specialCharRegex = /[@$!%*?&#]/g;
+    if (!regex.test(value)) return false;
+    const specialCharMatches = value.match(specialCharRegex);
+    if (!specialCharMatches || specialCharMatches.length !== 1) return false;
+    return true;
+  }
+  static _validateBirthdate(value) {
+    const today = new Date();
+    const birthdate = new Date(value);
+    let age = today.getFullYear() - birthdate.getFullYear();
+    const isBeforeBirthday = today.getMonth() < birthdate.getMonth() || (today.getMonth() === birthdate.getMonth() && today.getDate() < birthdate.getDate());
+    if (isBeforeBirthday) {
+      age -= 1;
+    }
+    return age >= CustomFormValidator._minimumAge;
+  }
+  static _validateCardNumber(value) {
+    value = value.replaceAll(" ", ""); // check empty
+    const regex = /^\d{16}$/;
+    return regex.test(value);
+  }
+  static _validateExpirationDate(value) {
+    if (!value) return false;
+    const expirationDate = new Date(value + "-01");
+    const thisMonth = new Date();
+    thisMonth.setDate(1);
+    thisMonth.setHours(0, 0, 0, 0);
+    return expirationDate >= thisMonth;
+  }
+  static _validateCVV(value) {
+    const regex = /^\d{3}$/;
+    return regex.test(value);
+  }
+}
+
+class CustomFormErrorHandler {
+  constructor(customForm) {
+    this._customForm = customForm;
+  }
+
+  setFormErrorMessage(message) {
+    this._setErrorMessage(this._customForm.$form, message);
+  }
+  clearFormErrorMessage() {
+    this._clearErrorMessage(this._customForm.$form);
+  }
+  setInputErrorMessage(inputElement, message) {
+    this._setErrorMessage(inputElement, message);
+  }
+  clearInputErrorMessage(inputElement) {
+    this._clearErrorMessage(inputElement);
+  }
+  _setErrorMessage(element, message) {
+    // need to change this for something else
+    let errorSpan = element.parentElement.querySelector(`.${element.localName}-error-message`);
+    if (!errorSpan) {
+      if (element.localName === "input") {
+        errorSpan = document.createElement("span");
+        errorSpan.classList.add(`${element.localName}-error-message`);
+        element.parentElement.appendChild(errorSpan);
+      } else if (element.localName === "form") {
+        errorSpan = document.createElement("span");
+        errorSpan.classList.add(`${element.localName}-error-message`);
+        element.appendChild(errorSpan);
       }
-    });
-    form.addEventListener("change", validateIfInput);
-    form.addEventListener("focusout", validateIfInput);
+    }
+    errorSpan.textContent = message;
+    if (element.localName === "input") element.classList.add("error");
+  }
+  _clearErrorMessage(element) {
+    let errorSpan = element.parentElement.querySelector(`.${element.localName}-error-message`);
+    if (errorSpan) errorSpan.remove();
+    if (element.localName === "input") element.classList.remove("error");
   }
 }
 
-/* END OF FORM VALIDATION */
+class CustomFormActionHandler {
+  _dictionary = { signup: (values) => window.auth.signUp(values), signin: (values) => window.auth.signin(values) };
 
-/* FORM ACTIONS */
-function addUser(values) {
-  const users = getUsers();
-  users.push(values);
-  setUsers(users);
-}
-function getUsers() {
-  return JSON.parse(localStorage.getItem(USERS_LIST_KEY) || "[]");
-}
-function setUsers(value) {
-  value = typeof value != "object" ? JSON.parse(value || "[]") : value;
-  localStorage.setItem(USERS_LIST_KEY, JSON.stringify(value));
-}
-function initUsers() {
-  setUsers(getUsers());
-}
+  constructor(customForm, errorHandler) {
+    this._customForm = customForm;
+    this._errorHandler = errorHandler;
+    this._action = this._customForm.$form.dataset.action;
+  }
 
-const actionsDictionary = {
-  signup: addUser
-};
-
-function getFormValues(form) {
-  const values = {};
-  form.querySelectorAll("input").forEach((input) => (values[input.name] = input.value));
-  return values;
-}
-function handleFormAction(form) {
-  const actionName = form.dataset.action;
-  const action = actionsDictionary[actionName];
-
-  if (action) {
-    action(getFormValues(form));
+  handleFormAction() {
+    try {
+      if (this._action in this._dictionary) {
+        this._dictionary[this._action](this._getFormValues());
+      } else throw new Error("Invalid form action");
+      this._errorHandler.clearFormErrorMessage();
+    } catch (error) {
+      this._errorHandler.setFormErrorMessage(error.message);
+    }
+  }
+  _getFormValues() {
+    const values = {};
+    this._customForm.$inputs.forEach((inputElement) => (values[inputElement.name] = inputElement.value));
+    return values;
   }
 }
 
-function init() {
+// init
+(function () {
   const forms = document.querySelectorAll(".custom-form");
-  forms.forEach((form) => attachFormsFunctionality(form));
-  initUsers();
-}
-
-init();
+  forms.forEach((form) => new CustomForm(form));
+})();
---
+++ scripts/users.js
@@ -0,0 +1,109 @@
+class Users {
+  _key = "listOfUsers";
+
+  constructor() {
+    this._init();
+  }
+
+  _init() {
+    this._setUsers(this._getUsers());
+  }
+  _getUsers() {
+    return JSON.parse(localStorage.getItem(this._key) || "[]");
+  }
+  _setUsers(value) {
+    value = typeof value != "object" ? JSON.parse(value || "[]") : value;
+    localStorage.setItem(this._key, JSON.stringify(value));
+  }
+  addUser(user) {
+    const users = this._getUsers();
+    users.push(user);
+    this._setUsers(users);
+  }
+  removeUser(email) {
+    let users = this._getUsers();
+    users = users.filter((user) => user.email != email);
+    this._setUsers(users);
+  }
+  getUser(email) {
+    return this._getUsers().find((user) => user.email === email);
+  }
+}
+
+class Auth {
+  _key = "loggedUser";
+
+  constructor(users) {
+    this._users = users;
+  }
+
+  signin(credentials) {
+    this._ensureNotLogged();
+    credentials.email = credentials.email.toLowerCase();
+    const dbUser = this._users.getUser(credentials.email);
+    if (!dbUser) throw new Error("Email not found");
+    if (credentials.password != dbUser.password) throw new Error("Wrong password");
+    this._setLoggedUser(credentials.email);
+    window.location.href = "./Dashboard.html";
+  }
+  signUp(userValues) {
+    this._ensureNotLogged();
+    userValues.email = userValues.email.toLowerCase();
+    const dbUser = this._users.getUser(userValues.email);
+    if (dbUser) throw new Error("Email already exists");
+    this._users.addUser(userValues);
+    this._setLoggedUser(userValues.email);
+    window.location.href = "./Dashboard.html";
+  }
+  getCurrentUserEmail() {
+    return localStorage.getItem(this._key);
+  }
+  isLogged() {
+    if (this.getCurrentUserEmail()) return true;
+    return false;
+  }
+  _setLoggedUser(email) {
+    localStorage.setItem(this._key, email);
+  }
+  _ensureNotLogged() {
+    if (this.isLogged()) throw new Error("Already logged in");
+  }
+}
+
+class ActiveUserData {
+  constructor(user, bodyElement) {
+    this._user = user;
+    this._bodyElement = bodyElement;
+    this._init();
+    this._fillData();
+  }
+
+  _init() {
+    this.$fields = Array.from(this._bodyElement.querySelectorAll("[data-user-field]"));
+  }
+  _fillData() {
+    this.$fields.forEach((element) => {
+      element.textContent = this._user[element.dataset.userField];
+    });
+  }
+}
+
+// init
+(function () {
+  window.users = new Users();
+  window.auth = new Auth(window.users);
+
+  const isLogged = window.auth.isLogged();
+  handleRestrictedPage(isLogged);
+
+  if (isLogged) {
+    new ActiveUserData(window.users.getUser(window.auth.getCurrentUserEmail()), document.body);
+  }
+})();
+
+function handleRestrictedPage(isLogged) {
+  const pageAuthSettings = document.body.dataset.auth;
+
+  if (!pageAuthSettings) return;
+  if ((pageAuthSettings === "true" && !isLogged) || (pageAuthSettings === "false" && isLogged)) window.location.href = "./Home.html";
+}
